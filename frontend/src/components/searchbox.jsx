275c9edc7a0361ba7966a9a7e59213468a0a001a
lau wei bin <lauweibin77@gmail.com>
2024-12-10 23:47:22 +0800

    finished with initial navbar design

--- frontend/src/components/searchbox.jsx
+++ frontend/src/components/searchbox.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 
 const searchbox = () => {
     return (
-        <div className="flex items-center  rounded-lg p-2 max-w-md mx-auto border-2 border-gray-200 gap-2 ">
-            <button ><GoSearch /></button>
-            <input placeholder="Search..." type="text" className=" rounded-xl w-80 h-8" />
+        <div className="flex items-center  rounded-lg p-2 max-w-md mx-auto  gap-2 ">
+            <input placeholder="Search..." type="text" className=" rounded-xl w-80 h-8 border-2 p-3" />
         </div>
     )
 }
