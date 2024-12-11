551bde8dde378aff3fc020e12ec92da4a2ce435a
lau wei bin <lauweibin77@gmail.com>
2024-12-11 23:30:28 +0800

    partially done with landing page. still need to add the instructions for what users can do below

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,12 +6,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
 
-    <div className="fixed scroll-m-0 top-0 font-bold w-screen flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center bg-transparent hover:bg-white">
+    <div className="fixed scroll-m-0 top-0 font-bold w-screen flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center backdrop-filter backdrop-blur-lg bg-opacity-30  hover:bg-white">
 
       {/* Logo and Text Linking to Homepage */}
       <Link to="/" className="flex gap-2 items-center">
         <img src={logo} alt="SG SPOTS" className="h-16 rounded-lg" />
-        <span>SG SPOTS</span>
+        <span className='text-white'>SG SPOTS</span>
       </Link>
 
       {/* Searchbox Component */}
@@ -21,12 +21,12 @@ const Navbar = () => {
       <div className="flex p-8 gap-8 h-16 items-center">
 
         {/* Log In Button */}
-        <Link to="/login" className="h-9 bg-gray-200 text-black font-bold rounded-xl px-4 py-2">
+        <Link to="/login" className="items-center inline-flex h-9 bg-gray-200 text-black font-bold rounded-xl px-4 py-2">
           Log In
         </Link>
 
         {/* Sign Up Button */}
-        <Link to="/signup" className="inline-flex h-9 bg-red-600 text-white font-bold rounded-xl px-4 py-2">
+        <Link to="/signup" className="  items-center inline-flex h-9 bg-red-600 text-white font-bold rounded-xl px-4 py-2">
           Sign Up
         </Link>
 
