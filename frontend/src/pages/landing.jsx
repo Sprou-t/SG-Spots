bd25293489aca9b840d635028e1ec03f8d104bc4
lau wei bin <lauweibin77@gmail.com>
2024-12-12 23:26:04 +0800

    almost done with home page, left with a minor gap in the image from 768px onwards

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -5,54 +5,44 @@ import writeImg from "../assets/images/garden-by-bay-night-scene-singapore.jpg"
 import { SlideLeft, SlideRight } from "../components/slideInAnimation.jsx";
 
 /* TODO: add the bottom sections
-			1. follow rateMyDorm's footer design
-			2. make everything responsive
+			2. make everything responsive(1279)
 			3. write a blog(share your experiences) 
-			4.*/
+			4. 832 when text size needs to change*/
 const Main = () => {
 	console.log("landing page loaded");
 	return (
-		<>
-			<div className="relative flex bg-red-200 h-screen w-screen mb-24">
-				<img src={mainImg} className="w-full" alt="Main" />
+		<div className="w-screen sm:p-4 m-0">
+			<div className="relative flex h-screen w-screen">
+				<img src={mainImg} className="w-screen object-cover m-0" alt="Main" />
 				<div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 text-6xl text-center font-semibold">
 					Discover the Best <span className="text-red-600">SG SPOTS</span> Here
 				</div>
 			</div>
 
-			<div className="w-4/5 m-auto grid gap-12">
-				<SlideRight customClass="flex gap-5">
-					<img src={findImg} className="size-9/12" alt="Find spots" />
-					<div className="flex flex-col gap-10">
+			<div className="sm:w-full md:w-4/5 flex flex-col gap-20 mx-auto py-20 m-0 xs:text-center xs:p-4">
+				<SlideRight customClass="flex gap-5 flex-col lg:flex-row items-center justify-center">
+					<img src={findImg} className="size-1/2" alt="Find spots" />
+					<div className="flex flex-col lg:gap-10 sm:gap-2">
 						<h2 className="text-2xl font-semibold">Explore Your SPOTS</h2>
 						<p>
-							We've gathered over 1000 amazing spots across Singapore just for you! Whether you're looking
-							for breathtaking views, delicious food, hidden gems, or vibrant entertainment, we’ve got you
-							covered. Discover the perfect places that match your mood and preferences, whether you're
-							feeling adventurous, relaxed, energetic, or contemplative. Our community-driven reviews and
-							ratings ensure you get the most authentic and up-to-date experiences, helping you explore,
-							enjoy, and create unforgettable memories.
+							We've curated over 1000 amazing spots across Singapore, from breathtaking views to delicious food and hidden gems. Explore places that match your mood, get community-driven insights, and enjoy authentic experiences that help you make unforgettable memories.
 						</p>
 					</div>
 				</SlideRight>
 
 
 
-				<SlideLeft customClass="flex gap-5">
-					<div className="flex flex-col gap-10">
+				<SlideLeft customClass="flex gap-5 flex-col-reverse lg:flex-row items-center justify-center">
+					<div className="flex flex-col lg:gap-10 sm:gap-2">
 						<h2 className="text-2xl font-semibold">Write an Anonymous Review</h2>
 						<p>
-							Share your unique experiences and insights with the community through anonymous reviews and
-							ratings! Help others discover their favorite spots by providing honest feedback based on
-							your visits. Whether it’s a hidden gem, a popular destination, or a place with room for
-							improvement, your contributions can make a difference. Your ratings and reviews help others
-							make informed decisions and create a vibrant, trusted space for sharing recommendations.
+							Share your experiences anonymously through reviews and ratings. Whether it’s a hidden gem or a popular spot, your honest feedback helps others make informed choices and fosters a trusted community of recommendations.
 						</p>
 					</div>
-					<img src={writeImg} className="size-9/12" alt="Write reviews" />
+					<img src={writeImg} className="size-1/2" alt="Write reviews" />
 				</SlideLeft>
 			</div >
-		</>
+		</div>
 
 	);
 }
