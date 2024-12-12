fed484ac6fce86fc2c24cc23c44913add9a62ec5
lau wei bin <lauweibin77@gmail.com>
2024-12-12 13:49:21 +0800

    finished main part of landing page

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,32 +6,32 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
 
-    <div className="fixed scroll-m-0 top-0 font-bold w-screen flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center backdrop-filter backdrop-blur-lg bg-opacity-30  hover:bg-white">
+    < div className="z-50 group fixed scroll-m-0 top-0 font-bold w-screen flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center backdrop-filter backdrop-blur-3xl bg-opacity-70  hover:bg-white" >
 
       {/* Logo and Text Linking to Homepage */}
-      <Link to="/" className="flex gap-2 items-center">
+      < Link to="/" className="flex gap-2 items-center" >
         <img src={logo} alt="SG SPOTS" className="h-16 rounded-lg" />
-        <span className='text-white'>SG SPOTS</span>
-      </Link>
+        <h1 className='text-white group-hover:text-black'><span className='text-red-600'>SG</span> SPOTS</h1>
+      </Link >
 
       {/* Searchbox Component */}
-      <Searchbox />
+      < Searchbox />
 
       {/* Navigation Buttons */}
-      <div className="flex p-8 gap-8 h-16 items-center">
+      < div className="flex p-8 gap-8 h-16 items-center" >
 
         {/* Log In Button */}
-        <Link to="/login" className="items-center inline-flex h-9 bg-gray-200 text-black font-bold rounded-xl px-4 py-2">
+        < Link to="/login" className="items-center inline-flex h-9 bg-gray-200 text-black font-bold rounded-xl px-4 py-2" >
           Log In
-        </Link>
+        </Link >
 
         {/* Sign Up Button */}
-        <Link to="/signup" className="  items-center inline-flex h-9 bg-red-600 text-white font-bold rounded-xl px-4 py-2">
+        < Link to="/signup" className="  items-center inline-flex h-9 bg-red-600 text-white font-bold rounded-xl px-4 py-2" >
           Sign Up
-        </Link>
+        </Link >
 
-      </div>
-    </div>
+      </div >
+    </div >
 
   )
 }
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,19 +1,48 @@
 import React from "react";
-import mainImage from "../assets/images/pexels-alaric-sim-380461-1029188.jpg"
+import mainImg from "../assets/images/pexels-alaric-sim-380461-1029188.jpg"
+import findImg from "../assets/images/water-new-night-marina-business.jpg"
+import writeImg from "../assets/images/garden-by-bay-night-scene-singapore.jpg"
 
-/* TODO: main section
-1. follow the webpage
-2. get some good pics
-3. design accordingly
-
-*/
+/* TODO: add the bottom sections
+			1. follow rateMyDorm's footer design
+			2. add animation for bottom part
+			3. write a blog(share your experiences) 
+			4.*/
 const Main = () => {
 	console.log("landing page loaded");
 	return (
-		<div className="flex bg-red-200 h-screen w-screen">
-			<img src={mainImage} className="w-full" />
-			<div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 text-6xl text-center font-semibold">Discover the Best <span className="text-red-600">SG SPOTS</span> Here</div>
-		</div>
+		<>
+			<div className="relative flex bg-red-200 h-screen w-screen mb-24">
+				<img src={mainImg} className="w-full" />
+				<div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 text-6xl text-center font-semibold">Discover the Best <span className="text-red-600">SG SPOTS</span> Here</div>
+			</div>
+
+			<div className="w-4/5 m-auto grid gap-12">
+				<div className="flex gap-5 ">
+					<img src={findImg} className="size-9/12" />
+					<div className="flex flex-col  gap-10">
+						<h2 className="text-2xl font-semibold">Explore Your SPOTS</h2>
+						<p>We've gathered over 1000 amazing spots across Singapore just for you! Whether you're looking for breathtaking views, delicious food, hidden gems, or vibrant entertainment, we’ve got you covered.
+							Discover the perfect places that match your mood and preferences, whether you're feeling adventurous, relaxed, energetic, or contemplative.
+							Our community-driven reviews and ratings ensure you get the most authentic and up-to-date experiences, helping you explore, enjoy, and create unforgettable memories. </p>
+					</div>
+
+				</div>
+				<div className="flex gap-5">
+					<div className="flex flex-col  gap-10">
+						<h2 className="text-2xl font-semibold">Write an Anonymous Review</h2>
+						<p>Share your unique experiences and insights with the community through anonymous reviews and ratings!
+							Help others discover their favorite spots by providing honest feedback based on your visits.
+							Whether it’s a hidden gem, a popular destination, or a place with room for improvement, your contributions
+							can make a difference. Your ratings and reviews help others make informed decisions and create a vibrant,
+							trusted space for sharing recommendations.</p>
+					</div>
+					<img src={writeImg} className="size-9/12" />
+
+				</div>
+			</div >
+		</>
+
 	);
 }
 
