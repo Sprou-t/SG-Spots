64fcbc4204e2f70a27975635ff984d094910d716
lau wei bin <lauweibin77@gmail.com>
2024-12-15 00:55:08 +0800

    completed layout for homepage. Not yet responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Homepage from "./pages/Homepage.jsx";
 import Landing from "./pages/Landing.jsx";
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
 	const [counter, setCounter] = useState(0);
