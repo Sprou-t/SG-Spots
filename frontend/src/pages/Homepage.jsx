7765e9cd8042f08eeff13d9251dc5cd1c280283d
lau wei bin <lauweibin77@gmail.com>
2024-12-15 18:47:02 +0800

    made homepage responsive

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+/*TODO:
+1. make the page responsive
+2. just by tapping the card to open each card */
+
 const AttractionCard = ({ attraction }) => {
     return (
         <div className="border-gray-300 border-solid border-2 flex flex-col gap-4  shadow-lg rounded-lg h-full bg-white">
             <div>
-                <img className="w-full max-h-72 object-cover rounded-t-lg" src={attraction.imageURL} alt={attraction.title} />
+                <img className="w-full md:max-h-32 lg:max-h-44 xl:max-h-72 object-cover rounded-t-lg" src={attraction.imageURL} alt={attraction.title} />
             </div>
             <div className="p-2 text-center flex flex-col flex-grow">
                 <h2 className="font-bold text-lg truncate">{attraction.title}</h2>
@@ -30,7 +34,7 @@ const Homepage = () => {
     }, []);
 
     return (
-        <div className="mb-20 mt-32 w-11/12 mx-auto translate-x-10 grid grid-cols-3 gap-10">
+        <div className=" xs:w-full  md:w-11/12 xl:w- mb-20 xs:mt-56 md:mt-32 mx-auto xl:translate-x-20 grid lg:grid-cols-3 md:grid-cols-2 gap-10">
             {attractions.map(attraction => (
                 <AttractionCard key={attraction.id} attraction={attraction} />
             ))}
