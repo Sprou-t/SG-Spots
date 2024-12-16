92f524fde1537018682110a6906ec60893f6a2a0
lau wei bin <lauweibin77@gmail.com>
2024-12-17 01:31:09 +0800

    added info for attractionPage w/o styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,41 +7,11 @@ import Footer from "./components/Footer.jsx";
 // import useScrollDirection from "./features/trackScrolling.jsx";
 
 const App = () => {
-	const oldScrollY = useRef(window.scrollY); // oldScrolly retains its value across renders
-
-	const [direction, setDirection] = useState('up');
-
-	const controlDirection = () => {
-		console.log('Current scrollY:', window.scrollY);
-		console.log('Previous scrollY:', oldScrollY.current);
-		if (window.scrollY > oldScrollY.current) {
-			setDirection('down');
-			console.log("scrolling down")
-		} else {
-			setDirection('up');
-			console.log("scrolling up")
-		}
-		oldScrollY.current = window.scrollY;
-		console.log(`direction: ${direction}`)
-	}
-
-	useEffect(() => {
-		console.log("Setting up scroll listener");
-
-		window.addEventListener('scroll', controlDirection, true);
-
-		return () => {
-			window.removeEventListener('scroll', controlDirection, true);
-			console.log('Scroll listener removed');
-		};
-	}, []);
-
-
 	return (
 		<div className=" h-[5000px] w-full">
 
-			<Navbar scrollDirection={direction} />
-			<Landing />
+			<Navbar />
+			<AttractionPage />
 			<Footer />
 		</div>
 	);
