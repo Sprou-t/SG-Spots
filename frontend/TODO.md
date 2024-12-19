b1550b8cd62329234fcac75122ec56b66cfd3b4c
lau wei bin <lauweibin77@gmail.com>
2024-12-19 14:00:31 +0800

    added routing btw the 2 forms

--- frontend/TODO.md
+++ frontend/TODO.md
@@ -1,8 +1,5 @@
 TODO:
 - TMR
-morning: create login and sign up page
-afternoon: add links for homepage + do routing
-
 morning: add navbar design + profile pic(follow airbnb where w/o user just an empty person's head silluoute) + a translation feature + blog + hamburger design
 afternoon: do comments layout using simulated data(1hr)
 extra: sign out option
--- frontend/src/pages/AttractionPage.jsx
+++ frontend/src/pages/AttractionPage.jsx
@@ -27,93 +27,52 @@ const AttractionPage = () => {
                 console.error('Error fetching attractions:', error);
             });
     }, [])
-    console.log(attraction)
-    // count the number of images
+    // console.log(attraction)
 
     if (attraction != null) {
         const imageCounter = attraction.imageURL.length
-        if (imageCounter == 1) {
-            return (
-                <div>
-                    <img src={attraction.imageURL[0]} alt="" />
-                    <div className='w-10/12 mx-auto my-14 gap-10 flex flex-col '>
-                        <div className='flex items-center text-4xl font-semibold text-gray-600 gap-6 justify-center gray-800'>
-                            <h2>{attraction.title}</h2>
-
-                        </div>
-                        <div className='flex gap-16 text-xl justify-center  border-2  p-10'>
-                            <div className='flex-col gap-6 flex'>
-                                <div className='flex items-center gap-3'>
-                                    <MdOutlineStarRate />
-                                    <div className=''>Rating: <span className='font-bold text-gray-600'>{attraction.rating}</span></div>
-                                </div>
-                                <div className='flex items-center gap-3'>
-                                    <RiMoneyDollarCircleFill />
-                                    <p>Price <span className='font-bold text-gray-600'>{attraction.pricing}</span></p>
-                                </div>
+        return (
+            <div>
+                {imageCounter > 1 ? <Carousel images={attraction.imageURL} /> : <img src={attraction.imageURL[0]} alt="" />}
+                <div className='w-10/12 mx-auto my-14 gap-10 flex flex-col items-center'>
+                    <div className='flex items-center text-4xl font-semibold text-gray-600 gap-6 justify-center gray-800'>
+                        <h2>{attraction.title}</h2>
+                        <CgWebsite />
+                    </div>
+                    <div className='md:flex gap-16 text-xl justify-center  border-2 xs:p-2 md:p-10'>
+                        <div className='flex-col gap-6 flex mb-6 md:mb-0'>
+                            <div className='flex items-center gap-3'>
+                                <MdOutlineStarRate />
+                                <p>Rating: <span className='font-bold text-gray-600'>{attraction.rating}</span></p>
                             </div>
-                            <div className='flex-col gap-6 flex'>
-                                <div className='flex items-center gap-3'>
-                                    <MdAccessTimeFilled />
-                                    <p className='font-bold text-gray-600'>{attraction.openingTime} - {attraction.closingTime}</p>
-                                </div>
-                                <div className='flex items-center gap-3'>
-                                    <FaLocationDot />
-                                    <p className='font-bold text-gray-600'> {attraction.address}</p>
-                                </div>
+                            <div className='flex items-center gap-1'>
+                                <RiMoneyDollarCircleFill />
+                                <p>Price <span className='font-bold text-gray-600'>{attraction.pricing}</span></p>
                             </div>
                         </div>
-                        <p className='text-lg w-7/12 leading-relaxed mx-auto'>{attraction.description}</p>
-                        <a href={attraction.website} target="_blank" rel="noopener noreferrer" className=' mx-auto text-red-600 hover:text-customRed-light underline text-lg font-semibold'>
-                            Find out more
-                        </a>
-                        <CommentSection />
-                    </div >
+                        <div className='flex-col gap-6 flex'>
+                            <div className='flex items-center gap-1'>
+                                <MdAccessTimeFilled />
+                                <p className='font-bold text-gray-600'>{attraction.openingTime} - {attraction.closingTime}</p>
+                            </div>
+                            <div className='flex items-center gap-1'>
+                                <FaLocationDot />
+                                <p className='font-bold text-gray-600'> {attraction.address}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className='text-lg w-7/12 leading-relaxed'>{attraction.description}</p>
+                    <a href={attraction.website} target="_blank" rel="noopener noreferrer">
+                        Find out more
+                    </a>
+                    <CommentSection />
                 </div >
-            )
-        } else if (imageCounter > 1) {
-            return (
-                <div>
-                    <Carousel images={attraction.imageURL} />
-                    <div className='w-10/12 mx-auto my-14 gap-10 flex flex-col items-center'>
-                        <div className='flex items-center text-4xl font-semibold text-gray-600 gap-6 justify-center gray-800'>
-                            <h2>{attraction.title}</h2>
-                            <CgWebsite />
-                        </div>
-                        <div className='md:flex gap-16 text-xl justify-center  border-2 xs:p-2 md:p-10'>
-                            <div className='flex-col gap-6 flex mb-6 md:mb-0'>
-                                <div className='flex items-center gap-3'>
-                                    <MdOutlineStarRate />
-                                    <p>Rating: <span className='font-bold text-gray-600'>{attraction.rating}</span></p>
-                                </div>
-                                <div className='flex items-center gap-1'>
-                                    <RiMoneyDollarCircleFill />
-                                    <p>Price <span className='font-bold text-gray-600'>{attraction.pricing}</span></p>
-                                </div>
-                            </div>
-                            <div className='flex-col gap-6 flex'>
-                                <div className='flex items-center gap-1'>
-                                    <MdAccessTimeFilled />
-                                    <p className='font-bold text-gray-600'>{attraction.openingTime} - {attraction.closingTime}</p>
-                                </div>
-                                <div className='flex items-center gap-1'>
-                                    <FaLocationDot />
-                                    <p className='font-bold text-gray-600'> {attraction.address}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <p className='text-lg w-7/12 leading-relaxed'>{attraction.description}</p>
-                        <a href={attraction.website} target="_blank" rel="noopener noreferrer">
-                            Find out more
-                        </a>
-                        <CommentSection />
-                    </div >
 
 
-                </div >
-            )
-        }
+            </div >
+        )
     }
 }
+
 
 export default AttractionPage
