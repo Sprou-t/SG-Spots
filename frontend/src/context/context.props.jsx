ba0ca170608bfc9b4fd73683e5202cf2002e7841
lau wei bin <lauweibin77@gmail.com>
2024-12-25 03:21:19 +0800

    added another form for review

---
+++ frontend/src/context/context.props.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+// propContext is used for every props that does not need to be drilled
+// in the future, need to create more context for more specific props
+export const PropsContext = createContext()
